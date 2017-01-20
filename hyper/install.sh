f739931326d716ee3e176b80c0bced47716dbbcf
Pablo Varela <pablovarela182@gmail.com>
2017-01-20 10:27:27 -0500

    Less code, more comments

--- hyper/install.sh
+++ hyper/install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 file_name='hyper.js'
-dir=''
 
-function go_to_script_dir {
-  pushd `dirname $0` > /dev/null
-  dir=`pwd -P`
-  popd > /dev/null
-}
+# get script directory
+pushd `dirname $0` > /dev/null
+dir=`pwd -P`
+popd > /dev/null
 
-function link_config_file {
-  ln -svf $dir/$file_name ~/.$file_name
-}
-
-go_to_script_dir && link_config_file
+# link config file
+ln -svf $dir/$file_name ~/.$file_name
--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# get script directory
 pushd `dirname $0` > /dev/null
 dir=`pwd -P`
 popd > /dev/null
 
-
+# loop in subdirectories
 for D in `find $dir -mindepth 1 -type d -not -path '*/\.*'`
 do
   bash $D/install.sh
 done
 
+# link repo
 ln -snvf $dir ~/.dotfiles
 

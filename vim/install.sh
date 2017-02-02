09407d2f638b9a081eef74942f3899f8b42e6517
Pablo Varela <pablovarela182@gmail.com>
2017-02-02 15:09:40 -0500

    Vi plug

--- vim/install.sh
+++ vim/install.sh
@@ -2,10 +2,16 @@
 
 file_name='vimrc'
 
+function preinstall {
+  curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+    > /dev/null
+}
+
 # get script directory
 pushd `dirname $0` > /dev/null
 dir=`pwd -P`
 popd > /dev/null
 
 # link config file
-ln -svf $dir/$file_name ~/.$file_name
+preinstall && ln -svf $dir/$file_name ~/.$file_name

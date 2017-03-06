a7b6cafc2825582b68af9cc8e5131758346c22fa
Pablo Varela <pablovarela182@gmail.com>
2017-03-06 12:59:25 -0500

    Now bash works on MacOS

--- bash/install.sh
+++ bash/install.sh
@@ -3,16 +3,17 @@
 os=`uname`
 file_name='bashrc'
 
-if [ ! $os == 'Darwin' ]; then
-  file_name='bash_profile'
-fi
-
 # get script directory
 pushd `dirname $0` > /dev/null
 dir=`pwd -P`
 popd > /dev/null
 
-# link config file
-ln -svf $dir/$file_name ~/.$file_name
+if [ $os == 'Darwin' ]; then 
+  ln -svf $dir/$file_name ~/.bash_profile
+else
+  # link config file
+  ln -svf $dir/$file_name ~/.$file_name
+fi
+
 file_name='inputrc'
 ln -svf $dir/$file_name ~/.$file_name

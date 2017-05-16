34f07e9e1caf85dbf42e49a74e515106a4d2f489
Pablo Varela <pablovarela182@gmail.com>
2017-05-16 10:07:23 -0400

    Changes to all

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -14,7 +14,7 @@ module.exports = {
     cursorColor: 'rgba(248,28,229,0.8)',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'UNDERLINE',
+    cursorShape: 'BLOCK',
 
     // set to true for blinking cursor
     cursorBlink: false,

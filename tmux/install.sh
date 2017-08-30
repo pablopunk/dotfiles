c465fdcd2b03214c92189a5489b3651c9914bd6b
Pablo Varela <pablovarela182@gmail.com>
2017-08-30 13:56:06 +0200

    Add tmux and more vim plugins

---
+++ tmux/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file_name='tmux.conf'
+
+# get script directory
+pushd `dirname $0` > /dev/null
+dir=`pwd -P`
+popd > /dev/null
+
+# link config file
+ln -svf $dir/$file_name ~/.$file_name

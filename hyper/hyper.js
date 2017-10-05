37e9e8f3e87f4e18d03828a0d114cb7c10c9e788
Pablo Varela <pablovarela182@gmail.com>
2017-10-06 00:10:28 +0200

    finally xo and standard together in love

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -110,7 +110,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperlinks", "hyper-one-dark"],
+  plugins: ["hyperlinks", "hyper-snazzy"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here

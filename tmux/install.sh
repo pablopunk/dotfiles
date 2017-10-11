a3622a6010b4cbc1ecaf6f543ffb7290247909e5
Pablo Varela <pablovarela182@gmail.com>
2017-10-11 02:15:23 +0200

    Organize vim plugins, beautiful tmux without plugins

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,14 +2,10 @@
 
 file_name='tmux.conf'
 
-function preinstall {
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-}
-
 # get script directory
 pushd `dirname $0` > /dev/null
 dir=`pwd -P`
 popd > /dev/null
 
 # link config file
-preinstall && ln -svf $dir/$file_name ~/.$file_name
+ln -svf $dir/$file_name ~/.$file_name

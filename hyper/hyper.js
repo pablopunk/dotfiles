afac508ca219c79834e06a3f379ede269ba90aa4
Pablo Varela <pablovarela182@gmail.com>
2018-08-13 23:56:10 +0200

    Update

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -111,7 +111,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperlinks', "hyperocean"],
+  plugins: ['hyperlinks', "hypercwd", "hyper-night-owl"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here

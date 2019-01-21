2973374d0c52d81aed6e55bddd12e738f2c457ae
pablopunk <pablovarela182@gmail.com>
2019-01-21 10:14:26 +0100

    mac

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -111,7 +111,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperlinks', "hypercwd", "nova-hyperterm"],
+  plugins: ['hyperlinks', "hypercwd", "hyperterm-gruvbox-dark"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here

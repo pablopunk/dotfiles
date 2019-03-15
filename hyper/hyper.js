1fab2a332ad5d43b11f3fb573c14091c7f8e2370
Pablo Varela <pablovarela182@gmail.com>
2019-03-15 23:09:20 +0100

    update

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -7,11 +7,11 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 15,
 
     // font family with optional fallbacks
     fontFamily:
-      '"Roboto Mono", Hack, "OperatorMono-Light", "SF Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      '"Dank Mono", Hack, "OperatorMono-Light", "SF Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
@@ -111,7 +111,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperlinks', "hypercwd", "hyperterm-gruvbox-dark"],
+  plugins: ['hyperlinks', "hypercwd", "hyper-snazzy"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here

f21c8f72f69bbe548d92a851ef6bdb5945063406
Pablo Varela <pablovarela182@gmail.com>
2019-04-18 02:20:31 +0200

    Fix coc

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-file_name='config/nvim/init.vim'
-
-function preinstall {
-  mkdir -p ~/.config/nvim
-}
-
-# get script directory
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# link config file
-preinstall && ln -svf $dir/$file_name ~/.$file_name
+mkdir -p ~/.config/nvim
+file_name='config/nvim/init.vim'
+ln -svf $dir/$file_name ~/.$file_name
+file_name='config/nvim/coc-settings.json'
+ln -svf $dir/$file_name ~/.$file_name

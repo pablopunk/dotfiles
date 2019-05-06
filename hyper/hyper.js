7662d6d16ef6c0d6e781a9a1958d955687de0ef8
Pablo Varela <pablovarela182@gmail.com>
2019-05-06 17:12:25 +0200

    update

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -89,7 +89,7 @@ module.exports = {
     bell: 'SOUND',
 
     // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
+    copyOnSelect: true,
 
     // if true, on right click selected text will be copied or pasted if no
     // selection is present (true by default on Windows)

609dd1be79d95dd5dc230b4e5e4a8269ba3e0b66
pablopunk <pablovarela182@gmail.com>
2019-05-22 14:16:26 +0200

    update

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -110,10 +110,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperlinks', "hypercwd"],
+  plugins: ['hyperlinks', "hypercwd", "hyper-native-dark"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: ['hyper-native-dark']
+  localPlugins: []
 }

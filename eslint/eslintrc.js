80d01ecc6a89658bbb786b0c83a0a1e017d3b610
pablopunk <pablovarela182@gmail.com>
2019-06-01 19:00:21 +0200

    Better eslint

--- eslint/eslintrc.js
+++ eslint/eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-    "extends": "standard"
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "standard",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };

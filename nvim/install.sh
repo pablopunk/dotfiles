f7adc2fe2d25a16b57d253e3194f621902dbbb5d
pablopunk <yo@pablo.pink>
2020-05-05 17:48:28 +0200

    nvim templates

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,7 +3,11 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p ~/.config/nvim
-file_name='config/nvim/init.vim'
-ln -svf $dir/$file_name ~/.$file_name
-file_name='config/nvim/coc-settings.json'
-ln -svf $dir/$file_name ~/.$file_name
+
+files=( "init.vim" "coc-settings.json" "templates" )
+
+for file_name in "${files[@]}"
+do
+  rm ~/.config/nvim/$file_name
+  ln -svf $dir/$file_name ~/.config/nvim/$file_name
+done

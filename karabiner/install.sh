d2bea88a88ea57732c9c09d2ef01942a42750112
pablopunk <pablo@pablopunk.com>
2021-01-04 11:39:44 +0100

    fix karabiner

--- karabiner/install.sh
+++ karabiner/install.sh
@@ -2,13 +2,8 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p ~/.config/karabiner
+mkdir -p ~/.config/
 
-files=( "karabiner.json" )
-
-for file_name in "${files[@]}"
-do
-  rm ~/.config/karabiner/$file_name
-  ln -svf $dir/$file_name ~/.config/karabiner/$file_name
-done
+rm -rf ~/.config/karabiner
+ln -svf $dir ~/.config/karabiner
 

e60bafbea73ec0fc44b2392f1eea2ea30f7e8613
Pablo P Varela <pablo@pablopunk.com>
2022-07-21 15:10:22 +0200

    awesome tmux

---
+++ tmux/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function preinstall {
+  rm -rf .tmux
+  mkdir -p .tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
+
+# get script directory
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# link config file
+preinstall && \
+  ln -svf $dir/tmux.conf ~/.tmux.conf

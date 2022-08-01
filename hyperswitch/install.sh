bbe8f114ed00640c5af15c40ded325448b811165
Pablo P Varela <pablo@pablopunk.com>
2022-08-01 22:49:46 +0200

    remove unused files

--- hyperswitch/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-preferences_dir="$HOME/Library/Preferences"
-file="com.bahoom.HyperSwitch.plist"
-mkdir -p "$preferences_dir"
-ln -svf "$dir/$file" "$preferences_dir/$file"
--- starship/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# get script directory
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# link config file
-mkdir -p ~/.config && ln -svf $dir/starship.toml ~/.config/starship.toml

3ba93b9840eb3d029ce818a3cf4d0c03c6674d81
Pablo P Varela <pablo@pablopunk.com>
2022-08-01 22:49:46 +0200

    remove unused files

--- alacritty/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# get script directory
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-mkdir -p "$HOME/.config/alacritty"
-ln -svf "$dir/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"

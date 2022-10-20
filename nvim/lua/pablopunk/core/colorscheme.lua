3b75c1f3a3bf8a5783f9d8fbd19dc9327f39f58c
Pablo P Varela <pablo@pablopunk.com>
2022-10-20 23:13:52 +0200

    dont scream if the colors are not there yet (first-install-friendly)

--- nvim/lua/pablopunk/core/colorscheme.lua
+++ nvim/lua/pablopunk/core/colorscheme.lua
@@ -1,6 +1,6 @@
 local handle = io.popen("defaults read -g AppleInterfaceStyle")
 if not handle then
-  vim.cmd("colorscheme onedark")
+  vim.cmd("silent! colorscheme onedark")
   return
 end
 
@@ -8,7 +8,7 @@ local result = handle:read("*a")
 handle:close()
 
 if string.match(result, "Dark") then
-  vim.cmd("colorscheme onedark")
+  vim.cmd("silent! colorscheme onedark")
 else
-  vim.cmd("colorscheme xcodelight")
+  vim.cmd("silent! colorscheme xcodelight")
 end

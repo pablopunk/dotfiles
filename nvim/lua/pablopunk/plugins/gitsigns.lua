23c1556e45bbdbf058f668cee9f8b6ffdf7830c3
Pablo P Varela <pablo@pablopunk.com>
2022-10-20 21:54:56 +0200

    i am the master of the universe. and nvim

---
+++ nvim/lua/pablopunk/plugins/gitsigns.lua
@@ -0,0 +1,37 @@
+require("gitsigns").setup({
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", ")", function()
+			if vim.wo.diff then
+				return ")"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		map("n", "(", function()
+			if vim.wo.diff then
+				return "("
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		-- Actions
+		map({ "n", "v" }, "<leader>ga", ":Gitsigns stage_hunk<CR>")
+		map({ "n", "v" }, "<leader>gA", ":Gitsigns undo_stage_hunk<CR>")
+		map({ "n", "v" }, "<leader>u", ":Gitsigns reset_hunk<CR>")
+	end,
+})

88aab3b98a95bb7db320866c80237c4387479d8b
Pablo P Varela <pablo@pablopunk.com>
2022-10-28 22:03:50 +0200

    wip

--- nvim/lua/pablopunk/plugins-setup.lua
+++ nvim/lua/pablopunk/plugins-setup.lua
@@ -98,6 +98,9 @@ return require("packer").startup(function(use)
   use("jayp0521/mason-null-ls.nvim")
   use("lukas-reineke/lsp-format.nvim") -- nice formatting config
 
+  -- debugging
+  use("mfussenegger/nvim-dap")
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then

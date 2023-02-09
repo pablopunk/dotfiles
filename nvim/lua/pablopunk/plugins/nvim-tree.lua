9a039b44a0b2a640e88544796e760e587ec18359
Pablo P Varela <pablo@pablopunk.com>
2023-02-09 12:28:50 +0100

    fix lazy nvim-tree

--- nvim/lua/pablopunk/plugins/nvim-tree.lua
+++ nvim/lua/pablopunk/plugins/nvim-tree.lua
@@ -4,8 +4,15 @@ if not setup then
 end
 
 -- recommended settings from nvim-tree docs
-vim.g.loaded = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
+-- fix for lazy-loading nvim-tree (we want it to open on startup)
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 nvimtree.setup {
   -- reload_on_bufenter = true,

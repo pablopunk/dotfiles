4fcdd6efa98be78136afa49556cce193a69f4362
Pablo P Varela <pablo@pablopunk.com>
2023-05-02 19:02:46 +0200

    Tmux custom themes saved here

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function preinstall {
-  rm -rf ~/.tmux
+  rm -rf ~/.tmux*
   mkdir -p ~/.tmux/plugins
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
@@ -12,3 +12,5 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # link config file
 preinstall && \
   ln -svf $dir/tmux.conf ~/.tmux.conf
+  ln -svf $dir/tmux-light.conf ~/.tmux-light.conf
+  ln -svf $dir/tmux-dark.conf ~/.tmux-dark.conf

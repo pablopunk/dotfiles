7a46bc68d7c3a8cf435a99e7926911e739825abb
Pablo P Varela <pablo@pablopunk.com>
2023-07-24 16:47:01 +0200

    separate lspsaga config

---
+++ nvim/lua/pablopunk/plugins/lsp/lspsaga.lua
@@ -0,0 +1,12 @@
+require("lspsaga").setup {
+  definition = {
+    edit = "<cr>",
+  },
+  finder = {
+    keys = {
+      toggle_or_open = "<cr>",
+      split = "s",
+      vsplit = "v",
+    },
+  },
+}

e7da9de78273a11cea5b5cb1455705bae912705a
Pablo P Varela <pablo@pablopunk.com>
2023-08-25 01:54:58 +0200

    forgot about lsp folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,9 @@ vim.loader.enable() -- Enables the experimental Lua module loader
 
 require "pablopunk.core.abbreviations"
 require "pablopunk.core.autocommands"
-require "pablopunk.core.colorscheme"
 require "pablopunk.core.commands"
 require "pablopunk.core.keymaps"
 require "pablopunk.core.options"
 
 require "pablopunk.lazy" -- plugins
+

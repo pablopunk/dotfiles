2be91a53a0c0e82f66c795ee522955139f3d6c12
Pablo P Varela <pablo@pablopunk.com>
2023-08-25 11:48:45 +0200

    sexy minimal alpha-nvim

--- nvim/lua/pablopunk/plugins/ui.lua
+++ nvim/lua/pablopunk/plugins/ui.lua
@@ -4,7 +4,27 @@ return {
     "goolord/alpha-nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("alpha").setup(require("alpha.themes.startify").config)
+      local alpha = require "alpha"
+      local dashboard = require "alpha.themes.dashboard"
+
+      -- Set header
+      dashboard.section.header.val = {
+        [[                               __                ]],
+        [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+        [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+        [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+        [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+        [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+        [[                                                 ]],
+        [[                                       @pablopunk]],
+      }
+
+      -- Set menu
+      dashboard.section.buttons.val = {
+        dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("r", " Recent", ":Telescope oldfiles<CR>"),
+      }
+      alpha.setup(dashboard.opts)
     end,
   },
   {

e03478e14c2ed37888178ad74f5cf1b1de365ac6
Pablo P Varela <pablo@pablopunk.com>
2023-09-03 13:04:00 +0200

    bring back GBrowse

--- nvim/lua/pablopunk/plugins/git.lua
+++ nvim/lua/pablopunk/plugins/git.lua
@@ -1,6 +1,12 @@
 return {
   "tpope/vim-fugitive", -- git tools
-  "tpope/vim-rhubarb", -- :GBrowse
+  {
+    "tpope/vim-rhubarb", -- :GBrowse
+    config = function()
+      -- I disabled netrw so I need Browse to use GBrowse
+      vim.cmd [[ command! -nargs=1 Browse silent execute '!open' shellescape(<q-args>,1) ]]
+    end,
+  },
   {
     "lewis6991/gitsigns.nvim", -- leftside git status
     config = function()

57ad810892ec676e917ecd43f4324eca03d1784b
Pablo P Varela <pablo@pablopunk.com>
2023-09-11 17:07:47 +0200

    which-key timeout again

--- nvim/lua/pablopunk/plugins/autocomplete.lua
+++ nvim/lua/pablopunk/plugins/autocomplete.lua
@@ -256,6 +256,10 @@ return {
     "folke/which-key.nvim", -- displays a popup with possible keybindings of the command you started typing
     config = function()
       local wk = require "which-key"
+
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300 -- triggers which-key faster
+
       wk.register {
         g = { name = "Go to (LSP)" },
         ["<leader>"] = {

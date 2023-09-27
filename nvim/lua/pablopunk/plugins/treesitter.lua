7505b6725ab485cc8deb8bb9852d43e17d714aa9
Pablo P Varela <pablo@pablopunk.com>
2023-09-27 20:03:20 +0200

    lazy load treesitter

--- nvim/lua/pablopunk/plugins/treesitter.lua
+++ nvim/lua/pablopunk/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter", -- syntax highlighting but complex or something like that
+    event = "BufRead",
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require("nvim-treesitter.configs").setup {

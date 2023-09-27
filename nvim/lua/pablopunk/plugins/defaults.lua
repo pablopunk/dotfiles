f8dc9c1bd238cc841d92410adfab8466e728f88a
Pablo P Varela <pablo@pablopunk.com>
2023-09-27 12:33:52 +0200

    order mini.nvim

--- nvim/lua/pablopunk/plugins/defaults.lua
+++ nvim/lua/pablopunk/plugins/defaults.lua
@@ -3,14 +3,16 @@ return {
     "echasnovski/mini.nvim", -- see plugins below for more info
     version = "*",
     config = function()
-      require("mini.splitjoin").setup {} -- `gS` split or join function arguments
-      require("mini.tabline").setup {} -- buffers as tabs
-      require("mini.surround").setup {} -- surround motion
       require("mini.comment").setup {} -- surround motion
+      require("mini.misc").setup {} -- useful functions (like zoom() below)
       require("mini.pairs").setup {} -- autopairs for (), {}, [], '', ""...
+      require("mini.splitjoin").setup {} -- `gS` split or join function arguments
       require("mini.starter").setup {} -- start screen
       require("mini.statusline").setup {} -- what do u think this is?
-      require("mini.misc").setup {} -- useful functions (like zoom() below)
+      require("mini.surround").setup {} -- surround motion
+      require("mini.tabline").setup {} -- buffers as tabs
+
+      -- mappings
       vim.keymap.set("n", "<leader>m", require("mini.misc").zoom, { silent = true, desc = "Maximize current buffer" })
     end,
   },

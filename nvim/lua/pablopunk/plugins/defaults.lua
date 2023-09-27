a27e68566ea93024dcc3211c093b3f90bf4adcba
Pablo P Varela <pablo@pablopunk.com>
2023-09-27 20:03:04 +0200

    lazy load some defaults

--- nvim/lua/pablopunk/plugins/defaults.lua
+++ nvim/lua/pablopunk/plugins/defaults.lua
@@ -16,10 +16,16 @@ return {
       vim.keymap.set("n", "<leader>m", require("mini.misc").zoom, { silent = true, desc = "Maximize current buffer" })
     end,
   },
-  "editorconfig/editorconfig-vim", -- editorconfig support
+  {
+    "editorconfig/editorconfig-vim", -- editorconfig support
+    event = { "BufReadPre", "BufNewFile" },
+  },
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "pablopunk/persistent-undo.vim", -- undo works across vim sessions
   "stefandtw/quickfix-reflector.vim", -- edits to quickfix will be saved to the actual file/line
   "christoomey/vim-tmux-navigator", -- move through vim splits & tmux with <C-hjkl>
-  "gcmt/wildfire.vim", -- keep pressing ENTER to select the inner object you're in
+  {
+    "gcmt/wildfire.vim", -- keep pressing ENTER to select the inner object you're in
+    event = { "BufReadPre", "BufNewFile" },
+  },
 }

aa7b25a0d6cd05aed3d560bbc0aedc579eb7cee6
Pablo P Varela <pablo@pablopunk.com>
2023-10-10 17:33:15 +0200

    romainl/vim-cool

--- nvim/lua/pablopunk/plugins/defaults.lua
+++ nvim/lua/pablopunk/plugins/defaults.lua
@@ -1,14 +1,15 @@
 return {
-  {
-    "editorconfig/editorconfig-vim", -- editorconfig support
-    event = { "BufReadPre", "BufNewFile" },
-  },
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "pablopunk/persistent-undo.vim", -- undo works across vim sessions
   "stefandtw/quickfix-reflector.vim", -- edits to quickfix will be saved to the actual file/line
   "christoomey/vim-tmux-navigator", -- move through vim splits & tmux with <C-hjkl>
+  "romainl/vim-cool", -- disables search highlighting when you are done searching and re-enables it when you search again
   {
     "gcmt/wildfire.vim", -- keep pressing ENTER to select the inner object you're in
+    event = { "BufReadPre", "BufNewFile" },
+  },
+  {
+    "editorconfig/editorconfig-vim", -- editorconfig support
     event = { "BufReadPre", "BufNewFile" },
   },
 }

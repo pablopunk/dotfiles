071988b47990a18ba95cef6a4e1c8da9acd3d5a3
Pablo P Varela <pablo@pablopunk.com>
2023-10-24 15:31:30 +0200

    fix telescope not working on latest neovim

--- nvim/lua/pablopunk/plugins/telescope.lua
+++ nvim/lua/pablopunk/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.0",
+    branch = "master",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-live-grep-args.nvim",

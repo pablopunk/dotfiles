bc652fa876a9d8b512187a1cfcc619104c46987e
Pablo P Varela <pablo@pablopunk.com>
2023-10-24 15:31:06 +0200

    copilot as a cmp source

--- nvim/lua/pablopunk/plugins/copilot.lua
+++ nvim/lua/pablopunk/plugins/copilot.lua
@@ -4,21 +4,21 @@ return {
     event = "InsertEnter",
     config = function()
       -- workaround for Tab not inserting a tab character https://github.com/zbirenbaum/copilot.lua/discussions/153#discussioncomment-5701223
-      vim.keymap.set("i", "<Tab>", function()
-        if require("copilot.suggestion").is_visible() then
-          require("copilot.suggestion").accept()
-        else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-        end
-      end, { desc = "Super Tab. Accept Copilot suggestion or insert a tab character if there's none" })
-
+      -- vim.keymap.set("i", "<Tab>", function()
+      --   if require("copilot.suggestion").is_visible() then
+      --     require("copilot.suggestion").accept()
+      --   else
+      --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+      --   end
+      -- end, { desc = "Super Tab. Accept Copilot suggestion or insert a tab character if there's none" })
+      --
       require("copilot").setup {
         panel = {
           enabled = false,
         },
         suggestion = {
-          enabled = true,
-          auto_trigger = true,
+          enabled = false, -- disabled to use copilot-cmp instead
+          auto_trigger = false, -- disabled to use copilot-cmp instead
         },
         filetypes = {
           yaml = true,
@@ -26,6 +26,18 @@ return {
           help = true,
           gitcommit = true,
           ["."] = true,
+        },
+      }
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp", -- Turn copilot into a cmp source. Config in ./lsp.lua
+    dependencies = { "onsails/lspkind.nvim" },
+    config = function()
+      require("copilot_cmp").setup()
+      require("lspkind").init {
+        symbol_map = {
+          Copilot = "", -- copilot icon
         },
       }
     end,

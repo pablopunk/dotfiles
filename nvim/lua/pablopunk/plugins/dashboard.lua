093138eadaa5eb94f1783bd70c461a47bb5826f0
Pablo P Varela <pablo@pablopunk.com>
2023-10-24 15:56:52 +0200

    dashboard again

---
+++ nvim/lua/pablopunk/plugins/dashboard.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      theme = "hyper",
+      config = {
+        week_header = {
+          enable = true,
+        },
+      },
+    },
+  },
+}

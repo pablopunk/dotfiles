e4dc27d632d266bd05758a4d7a408fdcd3af5e4d
Pablo P Varela <pablo@pablopunk.com>
2023-10-24 19:13:24 +0200

    bring back auto-session

--- nvim/lua/pablopunk/plugins/mini.lua
+++ nvim/lua/pablopunk/plugins/mini.lua
@@ -5,26 +5,24 @@ return {
     config = function()
       require("mini.comment").setup {} -- surround motion
       require("mini.pairs").setup {} -- autopairs for (), {}, [], '', ""...
-      require("mini.sessions").setup {}
       require("mini.splitjoin").setup {} -- `gS` split or join function arguments
       require("mini.starter").setup {} -- start screen
       require("mini.statusline").setup {} -- what do u think this is?
       require("mini.surround").setup {} -- surround motion
       require("mini.tabline").setup {} -- buffers as tabs
+
+      -- file tree
       require("mini.files").setup {
         mappings = {
           go_in_plus = "<CR>", -- <Enter> will open the file and close the explorer
           synchronize = "<c-s>", -- <c-s> will write the changes you make in the explorer
         },
-      } -- file tree
-
-      -- keymaps
+      }
       local minifiles_toggle = function(...)
         if not MiniFiles.close() then
           MiniFiles.open(...)
         end
       end
-
       vim.keymap.set("n", "<c-t>", minifiles_toggle, { desc = "Toggle file explorer" })
       vim.keymap.set(
         "n",

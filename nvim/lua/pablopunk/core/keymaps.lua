c70c322c366b91cc44e73a6bb2f75a632864eb8a
Pablo P Varela <pablo@pablopunk.com>
2023-10-27 21:33:36 +0200

    insert file name at cursor

--- nvim/lua/pablopunk/core/keymaps.lua
+++ nvim/lua/pablopunk/core/keymaps.lua
@@ -53,6 +53,7 @@ keymap.set({ "n", "v" }, "<c-s>", ":w!<cr>", opts "Save file")
 -- File path utils {{{
 keymap.set("n", "<leader>fpa", ":let @+ = expand('%:p')<cr>", opts "Copy file path (absolute)")
 keymap.set("n", "<leader>fpr", ":let @+ = expand('%:p:r')<cr>", opts "Copy file path (relative)")
+keymap.set("n", "<leader>fni", ":let @0 = expand('%:p:t')<cr>\"0p", opts "Insert file name")
 -- }}}
 
 -- System clipboard {{{

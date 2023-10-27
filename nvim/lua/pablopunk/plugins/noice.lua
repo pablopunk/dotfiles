b2a4ef54095e73ccb5da47cc8a9a748a7d3d3c56
Pablo P Varela <pablo@pablopunk.com>
2023-10-27 20:25:05 +0200

    noice again

--- nvim/lua/pablopunk/plugins/noice.lua
+++ nvim/lua/pablopunk/plugins/noice.lua
@@ -1,44 +1,44 @@
 return {
-  -- {
-  --   "folke/noice.nvim", -- wow this feels wrong. Gets rid of the command line and replaces it with a floating window and notifications
-  --   -- event = "VeryLazy",
-  --   dependencies = {
-  --     "MunifTanjim/nui.nvim",
-  --     "rcarriga/nvim-notify",
-  --   },
-  --   opts = {
-  --     presets = {
-  --       bottom_search = true, -- use a classic bottom cmdline for search
-  --       command_palette = false, -- position the cmdline and popupmenu together
-  --       long_message_to_split = true, -- long messages will be sent to a split
-  --       inc_rename = false, -- enables an input dialog for inc-rename.nvim
-  --       lsp_doc_border = true, -- add a border to hover docs and signature help
-  --     },
-  --     routes = {
-  --       {
-  --         filter = { -- don't show a message every time you save a file
-  --           event = "msg_show",
-  --           kind = "",
-  --           find = "written", -- (spanish only) this depends on the language
-  --         },
-  --         opts = { skip = true },
-  --       },
-  --       {
-  --         filter = { -- same as above but in spanish
-  --           event = "msg_show",
-  --           kind = "",
-  --           find = "escrit",
-  --         },
-  --         opts = { skip = true },
-  --       },
-  --       {
-  --         filter = { -- reroute long notifications to splits
-  --           event = "notify",
-  --           min_height = 15,
-  --         },
-  --         view = "split",
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "folke/noice.nvim", -- wow this feels wrong. Gets rid of the command line and replaces it with a floating window and notifications
+    -- event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      routes = {
+        {
+          filter = { -- don't show a message every time you save a file
+            event = "msg_show",
+            kind = "",
+            find = "written", -- (spanish only) this depends on the language
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = { -- same as above but in spanish
+            event = "msg_show",
+            kind = "",
+            find = "escrit",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = { -- reroute long notifications to splits
+            event = "notify",
+            min_height = 15,
+          },
+          view = "split",
+        },
+      },
+    },
+  },
 }

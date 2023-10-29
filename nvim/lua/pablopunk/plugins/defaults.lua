d49a374bfe828796e9e540fbcc1e2e043edb3acf
Pablo P Varela <pablo@pablopunk.com>
2023-10-29 13:12:37 +0100

    fix scroll lag issue

--- nvim/lua/pablopunk/plugins/defaults.lua
+++ nvim/lua/pablopunk/plugins/defaults.lua
@@ -5,19 +5,17 @@ return {
   "christoomey/vim-tmux-navigator", -- move through vim splits & tmux with <C-hjkl>
   "romainl/vim-cool", -- disables search highlighting when you are done searching and re-enables it when you search again
   "markonm/traces.vim", -- to show in real time what your :s commands will replace
+  "tpope/vim-surround", -- surround motion
   {
     "editorconfig/editorconfig-vim", -- editorconfig support
     event = { "BufReadPre", "BufNewFile" },
   },
   {
     "axkirillov/hbac.nvim",
+    event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
       threshold = 6, -- hbac will start closing unedited buffers once that number is reached
     },
-  },
-  {
-    "tpope/vim-surround", -- surround motion
-    event = "VeryLazy",
   },
 }

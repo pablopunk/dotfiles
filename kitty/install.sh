2e0b458b7f78dc9db5b42f860baaa0be67565c60
Pablo P Varela <pablo@pablopunk.com>
2023-11-03 22:54:20 +0100

    frappe -> mocha

--- kitty/install.sh
+++ kitty/install.sh
@@ -6,7 +6,7 @@ ln -svf "$(dirname $(realpath $0))"/kitty.conf ~/.config/kitty/kitty.conf
 themes_dir=~/.config/kitty/themes
 tmp_themes_dir=/tmp/kitty-catppuccin
 
-[ -f $themes_dir/frappe.conf ] && exit
+[ -f $themes_dir/mocha.conf ] && exit
 
 mkdir -p $themes_dir
 git clone --depth 1 https://github.com/catppuccin/kitty $tmp_themes_dir

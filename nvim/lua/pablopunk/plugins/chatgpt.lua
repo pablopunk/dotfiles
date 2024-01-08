0fc2a8a5a9d11d60401855546732146a7aa4fa13
Pablo P Varela <pablo@pablopunk.com>
2024-01-08 16:08:35 +0100

    keymaps

--- nvim/lua/pablopunk/plugins/chatgpt.lua
+++ nvim/lua/pablopunk/plugins/chatgpt.lua
@@ -8,7 +8,28 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup {
-      api_key_cmd = 'op read "op://Personal/OpenAI API Key/api key" --no-newline',
+      api_key_cmd = "echo -n $OPENAI_API_KEY",
+      edit_with_instructions = {
+        keymaps = {
+          close = "<C-c>",
+          accept = "<C-y>",
+          toggle_diff = "<C-d>",
+          toggle_settings = "<C-o>",
+          toggle_help = "<C-h>",
+          cycle_windows = "<Tab>",
+          use_output_as_input = "<C-i>",
+        },
+      },
+      popup_input = {
+        submit = "<CR>",
+      },
     }
+    vim.keymap.set("n", "<leader>cg", ":ChatGPT<cr>", { silent = true, noremap = true, desc = "Edit with ChatGPT" })
+    vim.keymap.set(
+      "v",
+      "<leader>cg",
+      ":ChatGPTEditWithInstructions<cr>",
+      { silent = true, noremap = true, desc = "ChatGPT" }
+    )
   end,
 }

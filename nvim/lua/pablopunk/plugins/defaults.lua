c1e3b5a248aff4640445aace0364894127d02e5e
Pablo P Varela <pablo@pablopunk.com>
2024-01-12 16:05:57 +0100

    no satellite

--- nvim/lua/pablopunk/plugins/defaults.lua
+++ nvim/lua/pablopunk/plugins/defaults.lua
@@ -4,7 +4,7 @@ return {
   "pablopunk/persistent-undo.vim", -- undo works across vim sessions
   "stefandtw/quickfix-reflector.vim", -- edits to quickfix will be saved to the actual file/line
   "christoomey/vim-tmux-navigator", -- move through vim splits & tmux with <C-hjkl>
-  "romainl/vim-cool", -- disables search highlighting when you are done searching and re-enables it when you search again
+  -- "romainl/vim-cool", -- disables search highlighting when you are done searching and re-enables it when you search again
   "markonm/traces.vim", -- to show in real time what your :s commands will replace
   "tpope/vim-surround", -- surround motion
   {

601536ef5e04c7d501ebc98e8830d8efc35e7b07
Pablo P Varela <pablo@pablopunk.com>
2024-01-15 12:46:48 +0100

    zellij

--- nvim/lua/pablopunk/plugins/defaults.lua
+++ nvim/lua/pablopunk/plugins/defaults.lua
@@ -4,6 +4,13 @@ return {
   "pablopunk/persistent-undo.vim", -- undo works across vim sessions
   "stefandtw/quickfix-reflector.vim", -- edits to quickfix will be saved to the actual file/line
   "christoomey/vim-tmux-navigator", -- move through vim splits & tmux with <C-hjkl>
+  {
+    "Lilja/zellij.nvim", -- like vim-tmux-navigator but for zellij
+    opts = {
+      vimTmuxNavigatorKeybinds = true,
+      replaceVimWindowNavigationKeybinds = true
+    },
+  },
   -- "romainl/vim-cool", -- disables search highlighting when you are done searching and re-enables it when you search again
   "markonm/traces.vim", -- to show in real time what your :s commands will replace
   "tpope/vim-surround", -- surround motion

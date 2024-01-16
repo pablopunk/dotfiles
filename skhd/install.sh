d833f475aa8775056fd37b148b7465bd87787a92
Pablo P Varela <pablo@pablopunk.com>
2024-01-16 11:07:23 +0100

    skhd & yabai

---
+++ skhd/install.sh
@@ -0,0 +1,3 @@
+
+mkdir -p ~/.config/skhd
+ln -svf "$(dirname $(realpath $0))"/skhdrc ~/.config/skhd/skhdrc
---
+++ yabai/install.sh
@@ -0,0 +1,3 @@
+
+mkdir -p ~/.config/yabai
+ln -svf "$(dirname $(realpath $0))"/yabairc ~/.config/yabai/yabairc

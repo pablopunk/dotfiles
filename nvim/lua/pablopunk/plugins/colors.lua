c58d10c8d37a7ab7f9511a696cc36dcbcb165a9a
Pablo P Varela <pablo@pablopunk.com>
2024-01-16 11:08:01 +0100

    tokyonight 😳

--- nvim/lua/pablopunk/plugins/colors.lua
+++ nvim/lua/pablopunk/plugins/colors.lua
@@ -1,20 +1,28 @@
 return {
+  -- {
+  --   "catppuccin/nvim", -- The best colorscheme
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     require("catppuccin").setup {
+  --       dim_inactive = {
+  --         enabled = true,
+  --         shade = "light",
+  --         percentage = 0.3,
+  --       },
+  --       integrations = {
+  --         mini = true,
+  --       },
+  --     }
+  --     vim.cmd "colorscheme catppuccin-mocha"
+  --   end,
+  -- },
   {
-    "catppuccin/nvim", -- The best colorscheme
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-      require("catppuccin").setup {
-        dim_inactive = {
-          enabled = true,
-          shade = "light",
-          percentage = 0.3,
-        },
-        integrations = {
-          mini = true,
-        },
-      }
-      vim.cmd "colorscheme catppuccin-mocha"
+      vim.cmd "colorscheme tokyonight"
     end,
   },
   {
@@ -23,14 +31,14 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.opt.background = "dark"
-        vim.cmd "colorscheme catppuccin-mocha"
+        vim.cmd "colorscheme tokyonight"
       end,
       set_light_mode = function()
         if vim.fn.has "mac" == 0 then
           return -- Don't change colors on linux
         end
         vim.opt.background = "light"
-        vim.cmd "colorscheme catppuccin-latte"
+        vim.cmd "colorscheme tokyonight-day"
       end,
     },
   },

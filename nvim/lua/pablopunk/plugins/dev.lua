e1314a098552915421ec9c367151c6e159956565
Pablo P Varela <pablo@pablopunk.com>
2024-01-24 14:38:25 +0100

    require telescope when needed

--- nvim/lua/pablopunk/plugins/dev.lua
+++ nvim/lua/pablopunk/plugins/dev.lua
@@ -1,8 +1,5 @@
 -- Plugins to develop plugins in Lua
 return {
   { "folke/neodev.nvim", config = true }, -- lsp for developing neovim plugins
-  {
-    "tjdevries/tree-sitter-lua", -- to generate docs in plugins
-    enabled = false, -- DON'T ENABLE IT. I only wanna install it and use it on plugins, not on my config
-  },
+  -- "tjdevries/tree-sitter-lua", -- to generate docs in plugins
 }

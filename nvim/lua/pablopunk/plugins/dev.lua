4cff0c898317a8066b0a99e418edaff795c6ec81
Pablo P Varela <pablo@pablopunk.com>
2024-01-25 19:16:18 +0100

    i'm a genius

--- nvim/lua/pablopunk/plugins/dev.lua
+++ nvim/lua/pablopunk/plugins/dev.lua
@@ -1,5 +1,8 @@
 -- Plugins to develop plugins in Lua
 return {
   { "folke/neodev.nvim", config = true }, -- lsp for developing neovim plugins
-  -- "tjdevries/tree-sitter-lua", -- to generate docs in plugins
+  {
+    "tjdevries/tree-sitter-lua",
+    build = "make build_parser && rsync -u ./build/parser.so ./parser/lua.so", -- fix while this is not merged https://github.com/tjdevries/tree-sitter-lua/pull/53
+  }, -- to generate docs in plugins
 }

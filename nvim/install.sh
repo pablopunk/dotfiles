47d8d8e0f309ae63a1bb6bba072d9af765e4ee0c
Pablo P Varela <pablo@pablopunk.com>
2024-01-26 00:44:12 +0100

    lazy update on install

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,4 +6,4 @@ rm -rf ~/.config/nvim
 ln -svf $dir ~/.config/nvim
 
 echo "Installing neovim plugins..."
-nvim --headless "+Lazy! install" +qa > /dev/null
+nvim --headless "+Lazy! install" "+Lazy! update" +qa > /dev/null

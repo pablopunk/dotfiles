5361fbf8e8ba3aabbc6b9a67f705d95df09b66c0
Pablo P Varela <pablo@pablopunk.com>
2024-01-27 02:27:52 +0100

    more mini and more lazy

--- nvim/lua/pablopunk/plugins/dev.lua
+++ nvim/lua/pablopunk/plugins/dev.lua
@@ -1,11 +1,11 @@
 -- Plugins to develop plugins in Lua
 return {
-  {
-    "nvim-lua/plenary.nvim", -- lua utils
-  },
-  { "folke/neodev.nvim", config = true }, -- lsp for developing neovim plugins
-  {
-    "tjdevries/tree-sitter-lua", -- to generate docs in plugins
-    build = "make dist",
-  },
+  -- {
+  --   "nvim-lua/plenary.nvim", -- lua utils
+  -- },
+  -- { "folke/neodev.nvim", config = true}, -- lsp for developing neovim plugins
+  -- {
+  --   "tjdevries/tree-sitter-lua", -- to generate docs in plugins
+  --   build = "make dist",
+  -- },
 }

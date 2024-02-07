17e328b2b0800bd9b6bd886aa9709b695100f5ed
Pablo P Varela <pablo@pablopunk.com>
2024-02-07 13:40:47 +0100

    wilder

--- bartender/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -f ~/Library/Preferences/com.surteesstudios.Bartender.plist
-mkdir -p ~/Library/Preferences
-ln -sf "$(dirname $(realpath $0))"/com.surteesstudios.Bartender.plist ~/Library/Preferences/com.surteesstudios.Bartender.plist
-
-
--- skhd/install.sh
+++
@@ -1,3 +0,0 @@
-
-mkdir -p ~/.config/skhd
-ln -sf "$(dirname $(realpath $0))"/skhdrc ~/.config/skhd/skhdrc

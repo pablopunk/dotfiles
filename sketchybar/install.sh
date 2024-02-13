bf7a04d80ddb7ed9fc089c23383dc9bdadef3d8c
Pablo P Varela <pablo@pablopunk.com>
2024-02-13 23:57:20 +0100

    sketchybar instructinos

--- sketchybar/install.sh
+++ sketchybar/install.sh
@@ -4,13 +4,18 @@ dir="$(dirname $(realpath $0))"
 
 # check macos
 if [[ $(uname) == "Darwin" ]]; then
-  brew tap FelixKratz/formulae > /dev/null 2>&1
-  brew install sketchybar > /dev/null 2>&1
-  brew install jq > /dev/null 2>&1
-  brew tap homebrew/cask-fonts > /dev/null 2>&1
-  brew install font-sf-pro > /dev/null 2>&1
-  brew install --cask sf-symbols > /dev/null 2>&1
-  curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.16/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf > /dev/null 2>&1
+  if [[ -z "$(brew list | grep -w sketchybar)" ]]; then
+    echo
+    echo -e "\033[31m⨯ sketchybar is not installed\033[0m"
+    echo
+    echo brew tap FelixKratz/formulae
+    echo brew install sketchybar jq
+    echo brew tap homebrew/cask-fonts
+    echo brew install font-sf-pro
+    echo brew install --cask sf-symbols
+    echo curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.16/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+    echo
+  fi
 fi
 
 rm -rf ~/.config/sketchybar

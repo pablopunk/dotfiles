a392f0ce66836bcd7a361523ff81ef50d0737eb9
Pablo P Varela <pablo@pablopunk.com>
2024-02-15 13:21:24 +0100

    install font and signin copilot

--- kitty/bootstrap.sh
+++ kitty/bootstrap.sh
@@ -3,4 +3,5 @@
 if [[ -z $(brew list | grep kitty) ]]; then
   brew install kitty
   kitty +kitten themes --reload-in=all "Catppuccin-Mocha"
+  brew install homebrew/cask-fonts/font-sf-mono-for-powerline
 fi

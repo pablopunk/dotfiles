544706f54e7a2e3b80d57688d4f595928b4315e9
Pablo P Varela <pablo@pablopunk.com>
2024-02-16 13:51:32 +0100

    create .disabled

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -4,4 +4,15 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+source "$CONFIG_DIR/colors.sh" # Loads all defined colors
+
+if [ $SELECTED = true ]; then
+  sketchybar --set $NAME background.drawing=on \
+                         background.color=$ACCENT_COLOR \
+                         label.color=$BAR_COLOR \
+                         icon.color=$BAR_COLOR
+else
+  sketchybar --set $NAME background.drawing=off \
+                         label.color=$ACCENT_COLOR \
+                         icon.color=$ACCENT_COLOR
+fi

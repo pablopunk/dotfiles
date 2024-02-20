f73a09aa2abff3fba00be4fbe55a3448dcb58cd3
Pablo P Varela <pablo@pablopunk.com>
2024-02-20 17:40:35 +0100

    gpt-4-turbo

--- nvim/.config/nvim/lua/pablopunk/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/pablopunk/plugins/chatgpt.lua
@@ -43,6 +43,23 @@ return {
           use_output_as_input = "<C-i>",
         },
       },
+      openai_params = {
+        model = "gpt-4-turbo-preview",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 300,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = "gpt-4-turbo-preview",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
       popup_input = {
         submit = "<CR>",
       },

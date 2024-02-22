514b1516ba0621b1ce068f20d4321c03fc09d254
Pablo P Varela <pablo@pablopunk.com>
2024-02-22 17:54:14 +0100

    -A

---
+++ .disabled/new-nvim/.config/nvim/lua/plugins/configs/copilot-config.lua
@@ -0,0 +1,16 @@
+return {
+  panel = {
+    enabled = false,
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+  },
+  filetypes = {
+    yaml = true,
+    markdown = true,
+    help = true,
+    gitcommit = true,
+    ["."] = true,
+  }
+}

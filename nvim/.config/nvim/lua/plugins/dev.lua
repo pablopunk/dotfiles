121b32cf00c2e243636f504417f9cc3e03505410
Pablo P Varela <pablo@pablopunk.com>
2024-02-23 11:46:55 +0100

    lua lsp

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,10 +1,6 @@
 -- Plugins to develop plugins in Lua
 return {
   -- {
-  --   "nvim-lua/plenary.nvim", -- lua utils
-  -- },
-  -- { "folke/neodev.nvim", config = true }, -- lsp for developing neovim plugins
-  -- {
   --   "tjdevries/tree-sitter-lua", -- to generate docs in plugins
   --   build = "make dist",
   -- },

66320b2b1a47dafe5ab4add4c7b9fd24c264c08e
Pablo P Varela <pablo@pablopunk.com>
2024-02-23 20:45:38 +0100

    come on

--- .disabled/new-nvim/.config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.loader.enable()
-
-require("core.opts").initial()
-
-local lazy_path = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-require("core.lazy-setup").lazy(lazy_path)
-
-require "plugins"
-
--- .disabled/new-nvim/.config/nvim/lua/plugins/configs/session.lua
+++
@@ -1,4 +0,0 @@
-return {
-  log_level = "error",
-  auto_session_suppress_dirs = { "/", "~/", "~/src", "~/Downloads", "~/Desktop" },
-}

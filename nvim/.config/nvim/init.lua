78f4118821def45b4d38d202c2aac126228d36e6
Pablo P Varela <pablo@pablopunk.com>
2024-02-23 11:54:15 +0100

    map leader before lazy loads

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,6 @@
 vim.loader.enable() -- Enables the experimental Lua module loader
 
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
 require "core"
 require "lazy-setup"

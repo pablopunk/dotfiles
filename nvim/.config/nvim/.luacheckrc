88af7765d13de30a6fee2f91bd7861dc14168221
Pablo P Varela <pablo@pablopunk.com>
2024-02-26 12:37:44 +0100

    luacheck

---
+++ nvim/.config/nvim/.luacheckrc
@@ -0,0 +1,17 @@
+-- vim: ft=lua tw=80
+
+-- Global objects defined by the C code
+read_globals = {
+  "vim",
+}
+
+globals = {
+  "vim.g",
+  "vim.b",
+  "vim.w",
+  "vim.o",
+  "vim.bo",
+  "vim.wo",
+  "vim.go",
+  "vim.env",
+}
--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -10,6 +10,7 @@ return {
       require("mason-tool-installer").setup {
         ensure_installed = {
           "prettier",
+          "eslint",
           "eslint_d",
           "stylua",
           "luacheck",
@@ -59,12 +60,23 @@ return {
       local lint = require "lint"
 
       lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
+        javascript = { "eslint_d", "eslint" },
+        typescript = { "eslint_d", "eslint" },
+        javascriptreact = { "eslint_d", "eslint" },
+        typescriptreact = { "eslint_d", "eslint" },
         lua = { "luacheck" },
       }
+
+      lint.linters.luacheck.args = { "" }
+
+      -- when to trigger lint
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
     end,
   },
 }

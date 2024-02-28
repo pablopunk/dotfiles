00e6783129868a310d21ef197357dacf6dc8617e
Pablo P Varela <pablo@pablopunk.com>
2024-02-28 17:16:17 +0100

    sorry wilder, again

--- nvim/.config/nvim/lua/plugins/wilder.lua
+++ nvim/.config/nvim/lua/plugins/wilder.lua
@@ -15,43 +15,43 @@ return {
     end,
     config = function()
       local wilder = require "wilder"
-      wilder.setup { modes = { ":", "/", "?" }, next_key = "<c-n>", previous_key = "<c-p>" }
+      wilder.setup { modes = { ":", "/", "?" } }
 
       vim.opt.wildmenu = false -- disable wildmenu because wilder is enough
 
-      wilder.set_option("pipeline", {
-        wilder.branch(
-          wilder.cmdline_pipeline {
-            fuzzy = 1,
-          },
-          wilder.vim_search_pipeline {
-            fuzzy = 1,
-          }
-        ),
-      })
-
-      local popupmenu_renderer = wilder.popupmenu_renderer {
-        highlighter = wilder.basic_highlighter(),
-        empty_message = wilder.popupmenu_empty_message_with_spinner(),
-        left = {
-          " ",
-          wilder.popupmenu_devicons(),
-          " ",
-        },
-        right = {
-          " ",
-          wilder.popupmenu_scrollbar(),
-        },
-      }
-
-      wilder.set_option(
-        "renderer",
-        wilder.renderer_mux {
-          [":"] = popupmenu_renderer,
-          ["/"] = popupmenu_renderer,
-          substitute = popupmenu_renderer,
-        }
-      )
+      -- wilder.set_option("pipeline", {
+      --   wilder.branch(
+      --     wilder.cmdline_pipeline {
+      --       fuzzy = 1,
+      --     },
+      --     wilder.vim_search_pipeline {
+      --       fuzzy = 1,
+      --     }
+      --   ),
+      -- })
+      --
+      -- local popupmenu_renderer = wilder.popupmenu_renderer {
+      --   highlighter = wilder.basic_highlighter(),
+      --   empty_message = wilder.popupmenu_empty_message_with_spinner(),
+      --   left = {
+      --     " ",
+      --     wilder.popupmenu_devicons(),
+      --     " ",
+      --   },
+      --   right = {
+      --     " ",
+      --     wilder.popupmenu_scrollbar(),
+      --   },
+      -- }
+      --
+      -- wilder.set_option(
+      --   "renderer",
+      --   wilder.renderer_mux {
+      --     [":"] = popupmenu_renderer,
+      --     ["/"] = popupmenu_renderer,
+      --     substitute = popupmenu_renderer,
+      --   }
+      -- )
     end,
   },
 }

07e4b548caea35d98753872a97b6d91c2e1d1c7b
Pablo P Varela <pablo@pablopunk.com>
2024-02-29 14:13:41 +0100

    neovide configs

--- nvim/.config/nvim/lua/plugins/neovide.lua
+++ nvim/.config/nvim/lua/plugins/neovide.lua
@@ -6,6 +6,14 @@ if vim.g.neovide then
   map({ "i", "c" }, "<D-v>", "<C-R>+", "Paste (neovide)") -- Paste insert and command mode
   map("t", "<D-v>", [[<C-\><C-N>"+P]], "Paste (neovide)") -- Paste terminal mode
   vim.g.neovide_transparency = 0.9
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_floating_blur_amount_x = 2
+  vim.g.neovide_floating_blur_amount_y = 2
+  vim.o.guifont = "SF Mono Powerline:h15" -- text below applies for VimScript
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
 end
 
 return {}

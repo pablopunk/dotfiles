d71be3c28472a23aad747338ad57e79f2034b8b0
Pablo P Varela <pablo@pablopunk.com>
2024-03-11 19:14:37 +0100

    make statusline work again with old nvim

--- nvim/.config/nvim/lua/core/statusline.lua
+++ nvim/.config/nvim/lua/core/statusline.lua
@@ -1,30 +1,27 @@
 local utils = require "core.utils"
+local hi = utils.hi
 
-local M = {}
+Statusline = {}
 
 local align_right = "%="
 
-local hi = function(highlight_group)
-  return function(text)
-    return "%#" .. highlight_group .. "#" .. text .. "%*"
-  end
-end
-
-M.create_statusline_string = function()
-  local filename = vim.fn.expand "%:p"
-  local path_segments = vim.split(filename, "/")
-  if #path_segments > 1 then
-    filename = path_segments[#path_segments - 1] .. "/" .. path_segments[#path_segments]
-  else
-    filename = path_segments[#path_segments]
-  end
+Statusline.active = function()
+  local filename = utils.get_filename_compact()
   local clients = utils.get_lsp_clients_string()
 
   vim.cmd [[ hi StatusLineClients guifg=#29a4bd guibg=#1f2335 ]]
 
-  return filename .. align_right .. hi "StatusLineClients"(clients)
+  return string.format("%s %s %s", filename, align_right, hi "StatusLineClients"(clients))
 end
 
-vim.o.statusline = "%!v:lua.require('core.statusline').create_statusline_string()"
+Statusline.inactive = function()
+  return "%t" -- just the file name
+end
 
-return M
+vim.cmd [[
+  augroup Statusline
+  au!
+  au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
+  au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
+  augroup END
+]]

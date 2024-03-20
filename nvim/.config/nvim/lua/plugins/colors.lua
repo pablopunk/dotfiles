4df4fd3513c08d76084bb9a93ba2f56a4d5abf66
Pablo P Varela <pablo@pablopunk.com>
2024-03-20 12:34:31 +0100

    smort

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,3 +1,13 @@
+local function dark()
+  vim.opt.background = "dark"
+  vim.cmd "colorscheme tokyonight-storm"
+end
+
+local function light()
+  vim.opt.background = "light"
+  vim.cmd "colorscheme tokyonight-day"
+end
+
 return {
   -- {
   --   "catppuccin/nvim", -- The best colorscheme
@@ -22,27 +32,42 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd "colorscheme tokyonight-storm"
+      if vim.fn.has "mac" == 0 then
+        dark()
+        return -- Don't change colors on linux
+      end
+
+      local theme_file_lines = vim.fn.readfile(vim.fn.expand "~/.theme")
+      if theme_file_lines and #theme_file_lines == 1 then
+        local theme = theme_file_lines[1]
+        if theme == "Light" then
+          light()
+          return
+        end
+      end
+
+      -- default
+      dark()
     end,
   },
-  {
-    "f-person/auto-dark-mode.nvim", -- Auto dark mode (macOS, linux, windows)
-    event = "VeryLazy",
-    opts = {
-      update_interval = 1000,
-      set_dark_mode = function()
-        vim.opt.background = "dark"
-        vim.cmd "colorscheme tokyonight-storm"
-      end,
-      set_light_mode = function()
-        if vim.fn.has "mac" == 0 then
-          return -- Don't change colors on linux
-        end
-        vim.opt.background = "light"
-        vim.cmd "colorscheme tokyonight-day"
-      end,
-    },
-  },
+  -- {
+  --   "f-person/auto-dark-mode.nvim", -- Auto dark mode (macOS, linux, windows)
+  --   event = "VeryLazy",
+  --   opts = {
+  --     update_interval = 1000,
+  --     set_dark_mode = function()
+  --       vim.opt.background = "dark"
+  --       vim.cmd "colorscheme tokyonight-storm"
+  --     end,
+  --     set_light_mode = function()
+  --       if vim.fn.has "mac" == 0 then
+  --         return -- Don't change colors on linux
+  --       end
+  --       vim.opt.background = "light"
+  --       vim.cmd "colorscheme tokyonight-day"
+  --     end,
+  --   },
+  -- },
   {
     "pablopunk/transparent.vim", -- Transparent background
     enabled = function()

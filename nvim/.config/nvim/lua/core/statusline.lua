37a3c4bbf7446a4e78c70549c01e6fe5f9355946
Pablo P Varela <pablo@pablopunk.com>
2024-03-20 15:24:36 +0100

    smort 2

--- nvim/.config/nvim/lua/core/statusline.lua
+++ nvim/.config/nvim/lua/core/statusline.lua
@@ -5,11 +5,29 @@ Statusline = {}
 
 local align_right = "%="
 
+local _color_highlights = false -- run fn only once
+local function color_highlights()
+  if _color_highlights then
+    return
+  end
+
+  ---@type 'dark' | 'light'
+  local theme = vim.api.nvim_get_option "background"
+
+  if theme == "dark" then
+    vim.cmd [[ hi StatusLineClients guifg=#29a4bd guibg=#1f2335 ]]
+  else
+    vim.cmd [[ hi StatusLineClients guifg=#29a4bd guibg=#e9e9ec ]]
+  end
+
+  _color_highlights = true
+end
+
 Statusline.active = function()
   local filename = utils.get_filename_compact()
   local clients = utils.get_lsp_clients_string()
 
-  vim.cmd [[ hi StatusLineClients guifg=#29a4bd guibg=#1f2335 ]]
+  color_highlights()
 
   return string.format("%s %s %s", filename, align_right, hi "StatusLineClients"(clients))
 end

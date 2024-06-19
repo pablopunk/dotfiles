eb2bb96dabca17cc4493f2148f9746c276ea4682
Pablo P Varela <pablo@pablopunk.com>
2024-06-19 22:49:24 +0200

    welcome mvim: minimal nvim

--- nvim/post-stow.sh
+++ nvim/post-stow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nvim --headless "+Lazy! install" "+Lazy! clean" +qa > /dev/null 2>&1
-
+nvim --headless "+Lazy! install" "+Lazy! clean" +qa > /dev/null 2>&1 # install neovim plugins
+NVIM_APPNAME=nvim nvim --headless +qa > /dev/null 2>&1 # install mvim plugins

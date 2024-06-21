aa306345aba975cd101acecdb3e9e563441a1ff7
Pablo P Varela <pablo@pablopunk.com>
2024-06-21 15:42:10 +0200

    fix post-stow in mvim

--- nvim/post-stow.sh
+++ nvim/post-stow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 nvim --headless "+Lazy! install" "+Lazy! clean" +qa > /dev/null 2>&1 # install neovim plugins
-NVIM_APPNAME=nvim nvim --headless +qa > /dev/null 2>&1 # install mvim plugins
+NVIM_APPNAME=mvim nvim --headless +qa > /dev/null 2>&1 # install mvim plugins

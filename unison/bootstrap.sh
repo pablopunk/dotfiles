16130ebcaf50f45cb86b971e1b6c12c5d705cf5c
Pablo P Varela <pablo@pablopunk.com>
2024-07-20 15:38:15 +0200

    batch (no questions at all)

--- unison/bootstrap.sh
+++ unison/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 brew_install unison
-unison preferences $HOME $HOME/.dotfiles/.unison -auto -silent -terse -i
+unison preferences $HOME $HOME/.dotfiles/.unison -auto -silent -terse -i -batch

0a2bd1301bc864c0e01495144da8cb6dd3600737
Pablo P Varela <pablo@pablopunk.com>
2024-10-08 20:50:18 +0200

    common

--- home-manager/pablopunk.nix
+++ home-manager/pablopunk.nix
@@ -1,33 +1,6 @@
 { config, pkgs, ... }:
 
 {
+  imports = [ ./common.nix ];
   home.username = "pablopunk";
-  home.stateVersion = "24.05";
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  home.packages = with pkgs; [
-    _1password
-    btop
-    mise
-    powerline-fonts
-  ];
-
-  imports = let
-    mod = path: ../modules + "/${path}.nix";
-  in [
-    (mod "binaries/binaries")
-    (mod "gh/gh")
-    (mod "git/git")
-    (mod "karabiner/karabiner")
-    (mod "neovim/neovim")
-    (mod "shell")
-    (mod "starship/starship")
-    (mod "tmux/tmux")
-    (mod "vim/vim")
-    (mod "wezterm/wezterm")
-    (mod "zed/zed")
-    (mod "zsh/zsh")
-    (mod "editorconfig/editorconfig")
-  ];
 }

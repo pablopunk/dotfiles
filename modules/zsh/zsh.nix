43f45876209626d244d8f316973d8696a7f50378
Pablo P Varela <pablo@pablopunk.com>
2024-10-08 23:54:42 +0200

    wip

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -2,10 +2,10 @@
 
 {
   home.packages = with pkgs; [ zsh zsh-autosuggestions zsh-syntax-highlighting ];
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true; # is this working?
-  };
+  # programs.zsh = {
+  #   enable = true;
+  #   autosuggestion.enable = true;
+  #   syntaxHighlighting.enable = true; # is this working?
+  # };
   home.file.".zshrc".source = ./zshrc;
 }

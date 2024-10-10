3d23c91590f44690f1502d8e11966bec7c0158f3
Pablo P Varela <pablo@pablopunk.com>
2024-10-10 23:31:39 +0200

    multiple configs

--- modules/cursor/init.lua
+++ modules/cursor/init.lua
@@ -1,7 +1,17 @@
 return {
   brew = { "cursor" },
   config = {
-    source = "./config",
-    output = "~/Library/Application Support/Cursor/User",
+    {
+      source = "./config/settings.json",
+      output = "~/Library/Application Support/Cursor/User/settings.json",
+    },
+    {
+      source = "./config/keybindings.json",
+      output = "~/Library/Application Support/Cursor/User/keybindings.json",
+    },
+    {
+      source = "./config/extensions.json",
+      output = "~/.cursor/extensions/extensions.json",
+    },
   },
 }

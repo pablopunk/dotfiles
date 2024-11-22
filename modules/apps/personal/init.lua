92e40497523967b0dcbd6e72f43bcadaa009b162
Pablo P Varela <pablo@pablopunk.com>
2024-11-22 20:29:39 +0100

    1piece and aldente in apps

--- modules/apps/personal/init.lua
+++ modules/apps/personal/init.lua
@@ -17,12 +17,21 @@ return {
     "whatsapp",
     "zed",
   },
+  wget = {
+    url = "https://app1piece.com/1Piece-4.2.1.zip",
+    output = "/Applications/1Piece.app",
+    zip = true,
+  },
   post_install = [[
+    open ~/.dotfiles/modules/apps/personal/1Piece.app-settings
+    open ~/.dotfiles/modules/apps/personal/AlDente.app-settings
+    open -a "1Piece"
     open -a "AlDente"
     open -a "AltTab"
     open -a "Karabiner-Elements"
     open -a "Raycast"
     open -a "Swift Shift"
     open -a "CleanShot X"
+    open -a "Visual Studio Code"
   ]],
 }

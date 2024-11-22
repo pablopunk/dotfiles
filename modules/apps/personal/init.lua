3d7c842b5944cfc8c0f57bb503bb410a2ec58270
Pablo P Varela <pablo@pablopunk.com>
2024-11-22 21:33:09 +0100

    import before opening

--- modules/apps/personal/init.lua
+++ modules/apps/personal/init.lua
@@ -23,15 +23,17 @@ return {
       zip = true,
     },
   },
+  -- I'm using defaults while I don't make dot compatible.
+  -- Search for an app id like so: defaults domains | tr ', ' '\n' | grep -i 1Piece
   post_install = [[
+    defaults import jp.fuji.1Piece ~/.dotfiles/modules/apps/personal/preferences/1Piece.plist
+    defaults import pl.maketheweb.cleanshotx ~/.dotfiles/modules/apps/personal/preferences/CleanShotX.plist
+    defaults import com.apphousekitchen.aldente-pro ~/.dotfiles/modules/apps/personal/preferences/AlDente.plist
     open -a "1Piece"
     open -a "AlDente"
     open -a "CleanShot X"
     open -a "Karabiner-Elements"
     open -a "Raycast"
     open -a "Swift Shift"
-    defaults import jp.fuji.1Piece ~/.dotfiles/modules/apps/personal/preferences/1Piece.plist
-    defaults import pl.maketheweb.cleanshotx ~/.dotfiles/modules/apps/personal/preferences/CleanShotX.plist
-    defaults import com.apphousekitchen.aldente-pro ~/.dotfiles/modules/apps/personal/preferences/AlDente.plist
-  ]], -- use defaults while I don't make dot compatible. To see app id: defaults domains | tr ', ' '\n' | grep -i 1Piece
+  ]],
 }

b012cdcee277671ff9034ceb59132ae842744eba
Pablo P Varela <pablo@pablopunk.com>
2024-12-05 11:33:06 +0100

    defaults

--- modules/apps/personal/init.lua
+++ modules/apps/personal/init.lua
@@ -23,12 +23,26 @@ return {
       zip = true,
     },
   },
+  defaults = {
+    {
+      plist = "./defaults/1Piece.plist",
+      app = "jp.fuji.1Piece",
+    },
+    {
+      plist = "./defaults/CleanShotX.plist",
+      app = "pl.maketheweb.cleanshotx",
+    },
+    {
+      plist = "./defaults/AlDente.plist",
+      app = "com.apphousekitchen.aldente-pro",
+    },
+  },
   -- I'm using defaults while I don't make dot compatible.
   -- Search for an app id like so: defaults domains | tr ', ' '\n' | grep -i 1Piece
   post_install = [[
-    defaults import jp.fuji.1Piece ~/.dotfiles/modules/apps/personal/preferences/1Piece.plist
-    defaults import pl.maketheweb.cleanshotx ~/.dotfiles/modules/apps/personal/preferences/CleanShotX.plist
-    defaults import com.apphousekitchen.aldente-pro ~/.dotfiles/modules/apps/personal/preferences/AlDente.plist
+    defaults import jp.fuji.1Piece ~/.dotfiles/modules/apps/personal/defaults/1Piece.plist
+    defaults import pl.maketheweb.cleanshotx ~/.dotfiles/modules/apps/personal/defaults/CleanShotX.plist
+    defaults import com.apphousekitchen.aldente-pro ~/.dotfiles/modules/apps/personal/defaults/AlDente.plist
     open -a "1Piece"
     open -a "AlDente"
     open -a "CleanShot X"

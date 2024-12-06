de8f66f1ad0834bc470c9d003d3207fea6647385
Pablo P Varela <pablo@pablopunk.com>
2024-12-06 15:32:53 +0100

    xml plist

--- modules/apps/personal/init.lua
+++ modules/apps/personal/init.lua
@@ -37,12 +37,7 @@ return {
       app = "com.apphousekitchen.aldente-pro",
     },
   },
-  -- I'm using defaults while I don't make dot compatible.
-  -- Search for an app id like so: defaults domains | tr ', ' '\n' | grep -i 1Piece
   post_install = [[
-    defaults import jp.fuji.1Piece ~/.dotfiles/modules/apps/personal/defaults/1Piece.plist
-    defaults import pl.maketheweb.cleanshotx ~/.dotfiles/modules/apps/personal/defaults/CleanShotX.plist
-    defaults import com.apphousekitchen.aldente-pro ~/.dotfiles/modules/apps/personal/defaults/AlDente.plist
     open -a "1Piece"
     open -a "AlDente"
     open -a "CleanShot X"

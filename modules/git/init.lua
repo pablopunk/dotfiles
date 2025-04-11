3020edee35bd4548c5cdddb6119bdc647651e37b
Pablo P Varela <pablo@pablopunk.com>
2025-04-11 17:50:21 +0200

    gh inside git

--- modules/git/init.lua
+++ modules/git/init.lua
@@ -1,7 +1,18 @@
 return {
-  brew = { "git", "git-delta" },
-  config = {
+  brew = {
+    "git",
+    "git-delta",
+    "gh",
+  },
+  config = {{
     source = "./gitconfig",
     output = "~/.gitconfig",
-  },
+  }, {
+    source = "./gh.yml",
+    output = "~/.config/gh/config.yml",
+  }},
+  post_install = [[
+    gh auth status | grep 'Logged in to github.com account' > /dev/null || gh auth login --web -h github.com
+    gh extension list | grep gh-copilot > /dev/null || gh extension install github/gh-copilot
+  ]],
 }

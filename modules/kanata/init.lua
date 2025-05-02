5198d589e3ca0036bf633a63f43545466e227d75
Pablo P Varela <pablo@pablopunk.com>
2025-05-03 01:50:47 +0200

    if that works on a fresh linux silverblue install i will donate my salary

--- modules/kanata/init.lua
+++ modules/kanata/init.lua
@@ -6,6 +6,15 @@ return {
         {
             source = "./kanata.conf",
             output = "~/.config/kanata/kanata.conf",
+        },
+        {
+            source = "./kanata.service",
+            output = "~/.config/kanata/kanata.service",
         }
-    }
+    },
+    post_install = [[
+sudo cp ~/.config/kanata/kanata.service /etc/systemd/system/kanata.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now kanata
+]],
 }

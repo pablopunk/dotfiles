bdf358ae2c0319c501b537ce7185ded19bd08be2
Pablo P Varela <pablo@pablopunk.com>
2025-05-03 03:42:51 +0200

    borders in aerospace

--- modules/apps/aerospace/init.lua
+++ modules/apps/aerospace/init.lua
@@ -2,9 +2,17 @@ return {
   os = { "macos" },
   brew = {
     "nikitabobko/tap/aerospace --cask",
+    "FelixKratz/formulae/borders",
   },
   config = {
-    source = "./aerospace.toml",
-    output = "~/.aerospace.toml",
+    {
+      source = "./aerospace.toml",
+      output = "~/.aerospace.toml",
+    },
+    {
+      source = "./bordersrc",
+      output = "~/.config/borders/bordersrc",
+    },
   },
+  post_install = "brew services restart borders",
 }
--- modules/kanata/init.lua
+++ modules/kanata/init.lua
@@ -1,18 +1,19 @@
 return {
-    brew = {
-        "kanata"
+  os = { "linux" },
+  brew = {
+    "kanata",
+  },
+  config = {
+    {
+      source = "./kanata.conf",
+      output = "~/.config/kanata/kanata.conf",
     },
-    config = {
-        {
-            source = "./kanata.conf",
-            output = "~/.config/kanata/kanata.conf",
-        },
-        {
-            source = "./kanata.service",
-            output = "~/.config/kanata/kanata.service",
-        }
+    {
+      source = "./kanata.service",
+      output = "~/.config/kanata/kanata.service",
     },
-    post_install = [[
+  },
+  post_install = [[
 sudo cp ~/.config/kanata/kanata.service /etc/systemd/system/kanata.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now kanata

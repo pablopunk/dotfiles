ceae8003321e8a0b5aaa18192804309af40eb7eb
Pablo P Varela <pablo@pablopunk.com>
2025-05-03 13:43:52 +0200

    better apple

--- modules/sketchybar/config/items/apple.lua
+++ modules/sketchybar/config/items/apple.lua
@@ -7,28 +7,17 @@ sbar.add("item", { width = 5 })
 
 local apple = sbar.add("item", {
   icon = {
-    font = { size = 16.0 },
+    font = { size = 15.0 },
     string = icons.apple,
-    padding_right = 8,
+    padding_right = 9,
     padding_left = 8,
   },
   label = { drawing = false },
   background = {
     color = colors.bg,
-    border_width = 1,
+    border_width = 0,
   },
   padding_left = 1,
-  padding_right = 1,
+  padding_right = 6,
   click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 0",
 })
-
--- Double border for apple using a single item bracket
-sbar.add("bracket", { apple.name }, {
-  background = {
-    color = colors.transparent,
-    height = 24,
-  },
-})
-
--- Padding item required because of bracket
-sbar.add("item", { width = 3 })

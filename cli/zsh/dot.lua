cea0f8dab905ed504f4fa3d16762808a02b5b22b
Pablo P Varela <pablo@pablopunk.com>
2025-07-20 14:05:44 +0200

    very important comment

--- cli/zsh/dot.lua
+++ cli/zsh/dot.lua
@@ -1,9 +1,9 @@
 return {
   check = "which eza && which fd && which jq && which tldr && which tree && which vim && which wget && which zoxide && which zsh",
   install = {
-    brew = "brew install eza fd jq tldr tree vim wget zoxide zsh",
     apt = "sudo apt install -y eza fd-find jq tldr tree vim wget zoxide zsh",
     dnf = "sudo dnf install -y eza fd-find jq tldr tree vim wget zoxide zsh",
+    brew = "brew install eza fd jq tldr tree vim wget zoxide zsh",
   },
   link = {
     ["./zshrc"] = "~/.zshrc",
@@ -12,6 +12,12 @@ return {
     ["./zshrc.d/path.sh"] = "~/.zshrc.d/path.sh",
     ["./zshrc.d/functions.sh"] = "~/.zshrc.d/functions.sh",
     ["./zshrc.d/lazy.sh"] = "~/.zshrc.d/lazy.sh",
+    -- The temptation to just link the directory
+    -- ["./zshrc.d"] = "~/.zshrc.d",
+    -- but it's not a good idea because sometimes
+    -- I wanna add ONLY LOCALLY files like .secrets.sh
+    -- to the .zshrc.d directory, and I don't want to
+    -- commit them to the repo.
   },
   postinstall = "curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh",
 }

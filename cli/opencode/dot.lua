05a3be9ea05bb2ee9c48a5fa3b4a4a78ad4c252a
fake <fake>
2025-07-21 17:31:53 +0200

    fix opencode

--- cli/opencode/dot.lua
+++ cli/opencode/dot.lua
@@ -2,8 +2,6 @@ return {
   check = "which opencode",
   install = {
     brew = "brew install sst/tap/opencode",
-    bun = "bun install -g opencode-ai@latest",
-    npm = "npm install -g opencode-ai@latest",
     curl = "VERSION=latest curl -fsSL https://opencode.ai/install | bash",
   },
   link = {

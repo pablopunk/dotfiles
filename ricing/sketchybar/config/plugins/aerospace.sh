059ff15aff5cd21084f49709ee5a9871e7c928d2
Pablo P Varela <pablo@pablopunk.com>
2025-07-22 14:17:22 +0200

    pretti

--- ricing/sketchybar/config/plugins/aerospace.sh
+++ ricing/sketchybar/config/plugins/aerospace.sh
@@ -8,7 +8,7 @@ CURRENT_WORKSPACE=$(aerospace list-workspaces --focused 2>/dev/null || echo "1")
 ESCAPED_WORKSPACE_ID=$(echo "$WORKSPACE_ID" | sed 's/[^a-zA-Z0-9]/_/g')
 
 if [ "$WORKSPACE_ID" = "$CURRENT_WORKSPACE" ]; then
-	sketchybar --set space.$ESCAPED_WORKSPACE_ID background.drawing=on label.color=0xff000000
+	sketchybar --set space.$ESCAPED_WORKSPACE_ID background.drawing=on background.color=0xffb4befe label.color=0xff000000 label.font.style=Bold
 else
-	sketchybar --set space.$ESCAPED_WORKSPACE_ID background.drawing=off label.color=0xffffffff
+	sketchybar --set space.$ESCAPED_WORKSPACE_ID background.drawing=off background.color=0xff1e1e2e label.color=0xffffffff label.font.style=Regular
 fi 

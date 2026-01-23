34d05ef252e85e204e79e2bfd9dbc96e43a240b7
Pablo P Varela <pablo@pablopunk.com>
2026-01-23 14:48:13 +0100

    fix clobber

--- config/zsh/zshrc.d/mise.sh
+++ config/zsh/zshrc.d/mise.sh
@@ -1,8 +1,8 @@
 if (( $+commands[mise] )); then
   _mise_cache="$HOME/.cache/zsh/mise.zsh"
-  if [[ ! -f "$_mise_cache" || "$commands[mise]" -nt "$_mise_cache" ]]; then
+  if [[ ! -f "$_mise_cache" || "${commands[mise]}" -nt "$_mise_cache" ]]; then
     mkdir -p "${_mise_cache:h}"
-    mise activate zsh > "$_mise_cache"
+    mise activate zsh >| "$_mise_cache"
   fi
   source "$_mise_cache"
 fi

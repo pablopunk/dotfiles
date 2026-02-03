6d73f7caa71068d2797783aaf9059984c518fb90
Pablo P Varela <pablo@pablopunk.com>
2026-02-03 12:15:03 +0100

    smart remove

--- config/zsh/zshrc.d/20-core-functions.sh
+++ config/zsh/zshrc.d/20-core-functions.sh
@@ -9,9 +9,26 @@ function rm {
     command rm "$@"
     return
   fi
-  going_to="/tmp/trash/$USER/$(date +%s%N)"
-  mkdir -p "$going_to"
-  mv -f "$@" "$going_to"
+  mkdir -p /tmp/trash
+  # get each argument and name it as path-to-file-that-will-be-deleted
+  for arg in "$@"; do
+    # check if path exists
+    if [[ ! -e "$arg" ]]; then
+      echo "rm: cannot remove '$arg': No such file or directory"
+      continue
+    fi
+    # example: i run `rm .gitignore` in a folder ~/src then the name will be Users-pablopunk-src-.gitignore so we need to get the full path of the file or directory
+    dirname_for_trash=$(realpath "$arg")
+    # now replace all / with -
+    dirname_for_trash=${dirname_for_trash//\//-}
+    # now remove the first -
+    dirname_for_trash=${dirname_for_trash#*-}
+    # create directory where all versions will live
+    mkdir -p "/tmp/trash/$dirname_for_trash"
+    # move it there with a specific version (date.now)
+    date_now=$(date +%Y-%m-%d_%H-%M-%S)
+    mv -f "$arg" "/tmp/trash/$dirname_for_trash/$date_now"
+  done
 }
 
 # Git commit with message

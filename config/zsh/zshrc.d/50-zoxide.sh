a1a658ab6cad0c0d0cd686148eb8c8af1ab1cc44
Pablo P Varela <pablo@pablopunk.com>
2026-03-03 16:44:52 +0100

    noclobber

--- config/zsh/zshrc.d/50-zoxide.sh
+++ config/zsh/zshrc.d/50-zoxide.sh
@@ -2,7 +2,7 @@ if (( $+commands[zoxide] )); then
   _zoxide_cache="$HOME/.cache/zsh/zoxide.zsh"
   if [[ ! -f "$_zoxide_cache" || "$commands[zoxide]" -nt "$_zoxide_cache" ]]; then
     mkdir -p "${_zoxide_cache:h}"
-    zoxide init zsh > "$_zoxide_cache"
+    zoxide init zsh >| "$_zoxide_cache"
   fi
   source "$_zoxide_cache"
 fi

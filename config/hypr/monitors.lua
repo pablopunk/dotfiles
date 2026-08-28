fc99c9da12ba3517d7fe3f3792b16c0f40892fa9
Pablo P Varela <pablo@pablopunk.com>
2026-08-28 22:08:33 +0200

    jo

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -5,7 +5,7 @@ local omarchy_gdk_scale = 1
 local omarchy_monitor_scale = 1.25
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
-hl.monitor({ output = "DP-3", mode = "2560x1440@144", position = "0x0", scale = omarchy_monitor_scale })
+hl.monitor({ output = "DP-3", mode = "2560x1440@99.95", position = "0x0", scale = omarchy_monitor_scale })
 
 -- Configure a specific monitor.
 -- hl.monitor({ output = "DP-2", mode = "2560x1440@144", position = "0x0", scale = 1 })

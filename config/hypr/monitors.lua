435a8b3b26aeea1722f9d70be1340f5fe4fd3454
Pablo P Varela <pablo@pablopunk.com>
2026-08-31 10:10:09 +0200

    hmmm

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -1,8 +1,8 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 -- List current monitors and supported resolutions with: hyprctl monitors all
 
-local omarchy_gdk_scale = 2
-local omarchy_monitor_scale = 1.6
+local omarchy_gdk_scale = 1
+local omarchy_monitor_scale = 1
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
 hl.monitor({ output = "DP-3", mode = "2560x1440@99.95", position = "0x0", scale = omarchy_monitor_scale })
